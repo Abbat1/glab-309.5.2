2d00887d7fcd8787647a761d55dc7b453217ce67
Abbat1 <cozzyyist@gmail.com>
2024-09-16 21:56:37 -0500

    glab_309_5_2

---
+++ glab_309_5_2/src/main/java/com/example/glab_309_5_2/Glab30952Application.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30952Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30952Application.class, args);
+	}
+
+}
---
+++ glab_309_5_2/src/test/java/com/example/glab_309_5_2/Glab30952ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30952ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
